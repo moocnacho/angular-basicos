7279dedd2a6d47ea10532b0126af120de156a8c7
nacho <nacho@quininac.es>
2022-09-24 12:53:01 +0200

    primera subida

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
